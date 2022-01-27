be5e302d656b2152c13323997a30bc8fd365fd40
Jagannath Mishra <jagannath.mishra@matrixintel.com>
2022-01-28 01:16:49 +0530

    registation page validation completed

--- yet2learn-tutorial-web-app/src/main/resources/static/assets/js/main.js
+++ yet2learn-tutorial-web-app/src/main/resources/static/assets/js/main.js
@@ -1,8 +1,26 @@
-/*jQuery(function(t){
-    "use strict"
+function validateLoginForm() {
+	console.log("login validation started");
+	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	let email = document.forms["loginForm"]["email"];
+	if (email.value == "") {
+		document.getElementById("valid-email").innerHTML = "Please enter your email address!";
+		email.focus();
+		return false;
+	}
+	if (!email.value.match(mailformat)) {
+		document.getElementById("valid-email").innerHTML = "You have entered an invalid email address!";
+		email.focus();
+		return false;
+	}
 
-});*/
+	let password = document.forms["loginForm"]["password"];
+	if (password.value == "") {
+		document.getElementById("valid-password").innerHTML = "Please enter your password!";
+		password.focus();
+		return false;
 
-$(document).ready(function(){
-				 $("#msgid").html("This is Hello World by JQuery");
-				});
+	}
+
+
+}
+
